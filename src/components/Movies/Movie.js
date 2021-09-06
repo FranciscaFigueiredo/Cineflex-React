d6fccd53fc99711b4b9502b75aeb244ab138f670
Nivea <nivea@respondeai.com.br>
2021-09-06 19:37:38 -0300

    preparando para o review

--- src/components/Movies/Movie.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function Movie({ id, title, posterURL, overview, releaseDate }) {
-    return (
-        <Link to={`/sessoes/${id}`} className="movie">
-            <img src={posterURL} alt=""/>
-        </Link> 
-    );
-}
